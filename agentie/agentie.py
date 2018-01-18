1e1801fb1dc979baf185503409719bc370c298b3
Adrian Grajdeanu <grajdeanuadrian@gmail.com>
2018-01-18 04:27:42 +0200

    Finished login for default admin and password

--- agentie/agentie.py
+++ agentie/agentie.py
@@ -1,18 +1,20 @@
-import os
+import os, hashlib
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from models import *
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(
     SQLALCHEMY_DATABASE_URI='postgresql://useragentie:pass@localhost:5432/agentie',
-    SECRET_KEY='development-key',
+    SECRET_KEY='dtqf46vsa!803gon_5w!nm4_&&oo2+#19t=9b$%-$^r_s5+pzx*',
     USERNAME='useragentie',
     PASSWORD='pass',
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
 ))
+app.secret_key='dtqf46vsa!803gon_5w!nm4_&&oo2+#19t=9b$%-$^r_s5+pzx*'
 db.init_app(app)
 
 
@@ -21,14 +23,35 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login/')
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        engine = create_engine('postgresql://useragentie:pass@localhost:5432/agentie')
+        conn = engine.connect()
+        stmt = text('SELECT * FROM "Admin"')
+        result = conn.execute(stmt)
+        row = result.fetchone()
+        hash_object = hashlib.sha256(request.form['password'])
+        hex_dig = hash_object.hexdigest()
+        if request.form['username'] == row[1].encode('utf-8') and hex_dig == row[2].encode('utf-8'):
+            conn.close()
+            session['logged-in'] = True
+            return redirect(url_for('admin'))
+        conn.close()
+    return redirect(url_for('login'))
 
 
-@app.route('/register/')
-def register():
-    return 'register page'
+@app.route('/logout/')
+def logout():
+    session.pop('logged-in', None)
+    return redirect(url_for('login'))
+
+
+@app.route('/administrator/')
+def administrator():
+    return 'admin page'
 
 
 @app.route('/search/')
